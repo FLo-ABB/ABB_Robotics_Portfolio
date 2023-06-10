7bd34864af9dead7b3f7fa14d5a9215f685326e9
MrCybernetic <florian.lobert@fr.abb.com>
2023-06-10 21:01:33 +0200

    refactored to decrease complexity

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,8 @@
-charts = [];
+let charts = [get_robots_chart('myChartArticulated', "Articulated"), get_robots_chart('myChartPalletizer', "Palletizer"), get_robots_chart('myChartSCARA', "SCARA"), get_robots_chart('myChartPicker', "Parallel"), get_robots_chart('myChartPaint', "Paint"), get_robots_chart('myChartCollaborative', "Collaboratives")];
 
-function robotsChart(IdDiv, product_type) {
-    let minReach = 0;
-    let maxReach = 0;
-    let minPayload = 0;
-    let maxPayload = 0;
+function get_robots_chart(IdDiv, product_type) {
+    let nbTotal = get_number_by_type(product_type);
+    let nbActual = 0;
     const ctx = document.getElementById(IdDiv).getContext('2d');
     const myChart = new Chart(ctx, {
         type: 'scatter',
@@ -14,15 +12,15 @@ function robotsChart(IdDiv, product_type) {
                 data: [],
                 backgroundColor: 'rgb(20, 20, 20)',
                 pointBackgroundColor: function (context) {
-                    var index = context.dataIndex;
+                    let index = context.dataIndex;
                     if (typeof context.dataset.data[index] !== 'undefined') {
                         return context.dataset.data[index].random_color;
                     }
                 },
                 pointRadius: function (context) {
-                    var index = context.dataIndex;
+                    let index = context.dataIndex;
                     if (typeof context.dataset.data[index] !== 'undefined') {
-                        if (context.dataset.data[index].show == true) {
+                        if (context.dataset.data[index].show) {
                             return 5;
                         } else {
                             return 0;
@@ -77,103 +75,56 @@ function robotsChart(IdDiv, product_type) {
             }
         }
     });
-    charts.push(myChart);
-    // Color Management
-    let nbTotal = 0;
-    let nbActual = 0;
-    for (let index = 0; index < myJson.items.length; index++) {
-        if ((myJson.items[index].product_type == product_type)) {
-            if (product_type == "Articulated") {
-                if (!(myJson.items[index].product_name == "IRB 460") && !(myJson.items[index].product_name == "IRB 760") && !(myJson.items[index].product_name == "IRB 660")) {
-                    nbTotal++
-                }
-            } else if (!(product_type == "Palletizer")) {
-                nbTotal++
-            }
-        }
-        if (product_type == "Palletizer") {
-            if ((myJson.items[index].product_name == "IRB 460") || (myJson.items[index].product_name == "IRB 760") || (myJson.items[index].product_name == "IRB 660")) {
-                nbTotal++
-            }
-        }
-    }
-    for (let index = 0; index < myJson.items.length; index++) {
-        if ((myJson.items[index].product_type == product_type)) {
-            randomColor = Math.floor((nbActual / nbTotal) * 180) * (nbActual % 2) + Math.floor(180 + ((nbActual + 1) / nbTotal) * 180) * ((nbActual + 1) % 2);
-            randomColor = "hsl(" + randomColor + ",100%,60%)";
-            if (!(myJson.items[index].product_name == "IRB 460") && !(myJson.items[index].product_name == "IRB 760") && !(myJson.items[index].product_name == "IRB 660")) {
-                for (let index2 = 0; index2 < myJson.items[index].variants.length; index2++) {
-                    myChart.data.datasets[0].data.push({
-                        y: myJson.items[index].variants[index2].reach * 1000,
-                        x: myJson.items[index].variants[index2].capacity,
-                        name: myJson.items[index].variants[index2].name,
-                        product_thumb: myJson.items[index].product_thumb,
-                        description: myJson.items[index].description,
-                        read_more_url: myJson.items[index].read_more_url,
-                        random_color: randomColor,
-                        controller: myJson.items[index].controller,
-                        show: true
-                    });
-                }
-            }
-            if (product_type == "Articulated") {
-                if (!(myJson.items[index].product_name == "IRB 460") && !(myJson.items[index].product_name == "IRB 760") && !(myJson.items[index].product_name == "IRB 660")) {
-                    nbActual++;
-                }
-            } else if (!(product_type == "Palletizer")) {
-                nbActual++;
-            }
-        }
-        if (product_type == "Palletizer") {
-            if ((myJson.items[index].product_name == "IRB 460") || (myJson.items[index].product_name == "IRB 760") || (myJson.items[index].product_name == "IRB 660")) {
-                randomColor = Math.floor((nbActual / nbTotal) * 180) * (nbActual % 2) + Math.floor(180 + ((nbActual + 1) / nbTotal) * 180) * ((nbActual + 1) % 2);
-                randomColor = "hsl(" + randomColor + ",100%,60%)";
-                for (let index2 = 0; index2 < myJson.items[index].variants.length; index2++) {
-                    myChart.data.datasets[0].data.push({
-                        y: myJson.items[index].variants[index2].reach * 1000,
-                        x: myJson.items[index].variants[index2].capacity,
-                        name: myJson.items[index].variants[index2].name,
-                        product_thumb: myJson.items[index].product_thumb,
-                        description: myJson.items[index].description,
-                        read_more_url: myJson.items[index].read_more_url,
-                        random_color: randomColor,
-                        controller: myJson.items[index].controller,
-                        show: true
-                    });
-                }
-                nbActual++;
-            }
-        }
-    }
-
-
-    for (let index = 0; index < myChart.data.datasets[0].data.length; index++) {
-        if ((minReach == 0) || (minReach >= myChart.data.datasets[0].data[index].y)) minReach = myChart.data.datasets[0].data[index].y * .9;
-        if ((minPayload == 0) || (minPayload >= myChart.data.datasets[0].data[index].x)) minPayload = myChart.data.datasets[0].data[index].x * .9;
-        if ((maxReach == 0) || (maxReach <= myChart.data.datasets[0].data[index].y)) maxReach = myChart.data.datasets[0].data[index].y * 2;
-        if ((maxPayload == 0) || (maxPayload <= myChart.data.datasets[0].data[index].x)) maxPayload = myChart.data.datasets[0].data[index].x * 2;
-    }
-
-    myChart.options.scales.y.min = minReach;
-    myChart.options.scales.x.min = minPayload;
-    myChart.options.scales.y.max = maxReach;
-    myChart.options.scales.x.max = maxPayload;
-
-    //Update the Chart
-    myChart.update();
+    myJson.items.filter(item => item.product_type === product_type || (product_type === "Palletizer" && ["IRB 460", "IRB 760", "IRB 660"].includes(item.product_name)))
+    .forEach(item => {
+        create_robot_point(item, myChart, nbTotal, nbActual);
+        nbActual++;
+    });
+    return resized_chart(myChart);
 }
 
-robotsChart('myChartArticulated', "Articulated");
-robotsChart('myChartCollaborative', "Collaboratives");
-robotsChart('myChartSCARA', "SCARA");
-robotsChart('myChartPalletizer', "Palletizer");
-robotsChart('myChartParallel', "Parallel");
-robotsChart('myChartPaint', "Paint");
+function create_robot_point(robot, chart, nb_total, nb_actual) {
+    let randomColor = `hsl(${Math.floor((nb_actual / nb_total) * 180) * (nb_actual % 2) + Math.floor(180 + ((nb_actual + 1) / nb_total) * 180) * ((nb_actual + 1) % 2)},100%,60%)`;
+    robot.variants.forEach(variant => {
+        chart.data.datasets[0].data.push({
+            y: variant.reach * 1000,
+            x: variant.capacity,
+            name: variant.name,
+            product_thumb: robot.product_thumb,
+            description: robot.description,
+            read_more_url: robot.read_more_url,
+            random_color: randomColor,
+            controller: robot.controller,
+            show: true
+        });
+    });
+}
 
-document.getElementById("defaultOpen").click();
+function resized_chart(chart) {
+    let minReach = 0;
+    let minPayload = 0;
+    let maxReach = 0;
+    let maxPayload = 0;
+    for (let data of chart.data.datasets[0].data) {
+        if ((minReach == 0) || (minReach >= data.y))  minReach = data.y * .9;
+        if ((minPayload == 0) || (minPayload >= data.x))  minPayload = data.x * .9;
+        if ((maxReach == 0) || (maxReach <= data.y)) maxReach = data.y * 2;
+        if ((maxPayload == 0) || (maxPayload <= data.x))  maxPayload = data.x * 2;
+    }
+    chart.options.scales.y.min = minReach;
+    chart.options.scales.x.min = minPayload;
+    chart.options.scales.y.max = maxReach;
+    chart.options.scales.x.max = maxPayload;
+    chart.update();
+    return chart;
+}
 
-function openRobotType(evt, robotType) {
-    var i, tabcontent, tablinks;
+function get_number_by_type(type) {
+    return myJson.items.filter(item => item.product_type === type || (type === "Palletizer" && ["IRB 460", "IRB 760", "IRB 660"].includes(item.product_name))).length;
+}
+
+function open_type_tab(evt, robotType) {
+    let i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
         tabcontent[i].style.display = "none";
@@ -186,39 +137,24 @@ function openRobotType(evt, robotType) {
     evt.currentTarget.className += " active";
 }
 
-function show_hide(checkbox) {
-    charts.forEach(element => {
-        element.data.datasets[0].data.forEach(subelement => {
-            if (document.getElementById("IRC5_checkbox").checked && document.getElementById("Omnicore_checkbox").checked) {
-                subelement.show = true;
-            } else if (!document.getElementById("IRC5_checkbox").checked && document.getElementById("Omnicore_checkbox").checked) {
-                omnicore_supported = false;
-                subelement.controller.forEach(controller => {
-                    if (controller.includes("OmniCore")) {
-                        omnicore_supported = true;
-                    }
-                });
-                if (omnicore_supported) {
-                    subelement.show = true;
-                } else {
-                    subelement.show = false;
-                }
-            } else if (document.getElementById("IRC5_checkbox").checked && !document.getElementById("Omnicore_checkbox").checked) {
-                irc5_supported = false;
-                subelement.controller.forEach(controller => {
-                    if (controller.includes("IRC5")) {
-                        irc5_supported = true;
-                    }
-                });
-                if (irc5_supported) {
-                    subelement.show = true;
-                } else {
-                    subelement.show = false;
-                }
-            } else {
-                subelement.show = false;
-            }
-        });
-        element.update();
+function filter_controller() {
+    const irc5Checked = document.getElementById("IRC5_checkbox").checked;
+    const omnicoreChecked = document.getElementById("Omnicore_checkbox").checked;
+    
+    charts.forEach(chart => {
+      chart.data.datasets[0].data.forEach(data => {
+        let show = false;
+        if (irc5Checked && omnicoreChecked) {
+          show = true;
+        } else if (!irc5Checked && omnicoreChecked) {
+          show = data.controller.some(controller => controller.includes("OmniCore"));
+        } else if (irc5Checked && !omnicoreChecked) {
+          show = data.controller.some(controller => controller.includes("IRC5"));
+        }
+        data.show = show;
+      });
+      chart.update();
     });
-}
+  }
+
+document.getElementById("defaultOpen").click();
