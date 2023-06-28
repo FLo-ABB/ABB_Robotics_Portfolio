5430ceea40c9c800d80a252d8c58c83f6729b224
Florian LOBERT <florian.lobert@fr.abb.com>
2023-06-28 23:00:34 +0200

    refactoring

--- assets/script.js
+++ assets/script.js
@@ -210,45 +210,4 @@ function getJsonVariantSorted(json, sortBy = "payload") {
     return json;
 }
 
-function generateDistinctColors(numColors) {
-    const colors = [];
-    const goldenRatio = 0.618033988749895;
-    let hue = 0.8;
-
-    for (let i = 0; i < numColors; i++) {
-        hue += goldenRatio;
-        hue %= 1;
-        const color = hsvToRgb(hue, 0.95, 0.95); // value mean
-        colors.push(`rgb(${color[0]}, ${color[1]}, ${color[2]})`);
-    }
-
-    return colors;
-}
-
-function hsvToRgb(h, s, v) {
-    let r, g, b;
-    const i = Math.floor(h * 6);
-    const f = h * 6 - i;
-    const p = v * (1 - s);
-    const q = v * (1 - f * s);
-    const t = v * (1 - (1 - f) * s);
-
-    switch (i % 6) {
-        case 0:
-            r = v; g = t; b = p; break;
-        case 1:
-            r = q; g = v; b = p; break;
-        case 2:
-            r = p; g = v; b = t; break;
-        case 3:
-            r = p; g = q; b = v; break;
-        case 4:
-            r = t; g = p; b = v; break;
-        case 5:
-            r = v; g = p; b = q; break;
-    }
-
-    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)];
-}
-
 document.getElementById("defaultOpen").click();
